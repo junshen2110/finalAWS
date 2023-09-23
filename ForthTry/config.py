8cf3d730b3b6cef535b85fe8965118951a3bdf06
junshen2110 <143473514+junshen2110@users.noreply.github.com>
2023-09-23 15:48:19 +0800

    Add files via upload

---
+++ ForthTry/config.py
@@ -0,0 +1,8 @@
+customhost = "employee.cxu6wetzoblv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "looichenyong-employee"
+customregion = "us-east-1"
+
+
